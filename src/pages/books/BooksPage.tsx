b71d76bce4d737531db895a408406546547559c6
pramod kamble <kamblepramod806@gmail.com>
2024-04-28 17:37:05 +0530

    add book table

--- src/pages/books/BooksPage.tsx
+++ src/pages/books/BooksPage.tsx
@@ -1,8 +1,144 @@
+import { Badge } from "@/components/ui/badge";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { DropdownMenu } from "@/components/ui/dropdown-menu";
+import { LoaderCircle, MoreHorizontal } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+import { getBooks } from "@/http/api";
 
 const BooksPage = () => {
-    return (
-        <div>Books Page</div>
-    )
-}
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["books"],
+    queryFn: getBooks,
+    staleTime: 10000, // in miliseconds
+  });
 
-export default BooksPage
+  return (
+    <div>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Books</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <Card className="mt-5">
+        <CardHeader>
+          <CardTitle>Books</CardTitle>
+          <CardDescription>
+            Manage your books and view their sales performance.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="hidden w-[100px] sm:table-cell">
+                  <span className="">Image</span>
+                </TableHead>
+                <TableHead>Title</TableHead>
+                <TableHead>Genre</TableHead>
+                <TableHead className="hidden md:table-cell">
+                  Auther Name
+                </TableHead>
+                <TableHead className="hidden md:table-cell">
+                  Created at
+                </TableHead>
+                <TableHead>
+                  <span className="">Actions</span>
+                </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {data?.map((book) => {
+                return (
+                  <TableRow key={book._id}>
+                    <TableCell className="hidden sm:table-cell">
+                      <img
+                        alt="Product image"
+                        className="aspect-square rounded-md object-cover"
+                        height="64"
+                        src={book.coverImage}
+                        width="64"
+                      />
+                    </TableCell>
+                    <TableCell className="font-medium">{book.title}</TableCell>
+                    <TableCell>
+                      <Badge variant="secondary">{book.genre}</Badge>
+                    </TableCell>
+                    <TableCell className="hidden md:table-cell">
+                      {book.author.name}
+                    </TableCell>
+                    <TableCell className="hidden md:table-cell">
+                      {book.createdAt}
+                    </TableCell>
+                    <TableCell>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button
+                            aria-haspopup="true"
+                            size="icon"
+                            variant="ghost"
+                          >
+                            <MoreHorizontal className="h-4 w-4" />
+                            <span className="sr-only">Toggle menu</span>
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                          <DropdownMenuItem>Edit</DropdownMenuItem>
+                          <DropdownMenuItem>Delete</DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </CardContent>
+        <CardFooter>
+          <div className="text-xs text-muted-foreground">
+            Showing <strong>1-10</strong> of <strong>32</strong> products
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default BooksPage;
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,26 +1,3 @@
-import { useQuery } from "@tanstack/react-query";
-import { getBooks } from "@/http/api";
-import { Book } from "@/types";
-
 export default function HomePage() {
-  // Queries
-  const query = useQuery<Book[], Error>({
-    queryKey: ["todos"],
-    queryFn: getBooks,
-  });
-
-  return (
-    <div>
-      Home page
-      <br />
-      {query.isLoading
-        ? "Loding users..."
-        : Array.isArray(query.data)
-        ? query.data.map((book: Book) => {
-            return <li key={book._id}>{book.title}</li>;
-          })
-        : "No users found"}
-      <ul></ul>
-    </div>
-  );
+  return <div>Home page</div>;
 }
